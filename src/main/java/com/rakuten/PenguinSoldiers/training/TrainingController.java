d89b80224044e06b6d5f4017de069c1a3c106a08
dkonayuki <dkonayuki@gmail.com>
2015-04-27 19:12:07 +0900

    create training entity, repository

--- src/main/java/com/rakuten/PenguinSoldiers/training/TrainingController.java
+++ src/main/java/com/rakuten/PenguinSoldiers/training/TrainingController.java
@@ -1,15 +1,29 @@
 package com.rakuten.PenguinSoldiers.training;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TrainingController {
 	
 	@RequestMapping(value = "trainings", method = RequestMethod.GET)
-	public String index(Principal principal) {
+	public String index(Principal principal, Model model) {
+    // Here we are returning a collection of Training objects
+		//List<Training> trainings = new ArrayList();
+		model.addAttribute("test", "");
 		return "training/trainingHome";
 	}
+	
+
+	/*@RequestMapping(value = "message", method = RequestMethod.GET)
+	public String messages(Model model) {
+	    model.addAttribute("messages", messageRepository.findAll());
+	    return "message/list";
+	}*/
 }
