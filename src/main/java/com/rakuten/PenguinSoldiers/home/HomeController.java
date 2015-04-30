b9960e8b5164d82be30915fb68d341d958f056cb
gUrban <gurban.hudayberdiyev@rakuten.com>
2015-04-30 17:51:24 +0900

    enable logging

--- src/main/java/com/rakuten/PenguinSoldiers/home/HomeController.java
+++ src/main/java/com/rakuten/PenguinSoldiers/home/HomeController.java
@@ -2,14 +2,19 @@ package com.rakuten.PenguinSoldiers.home;
 
 import java.security.Principal;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
 	
+	public static final Logger logger = Logger.getLogger(HomeController.class);
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Principal principal) {
+		
+		logger.info("Entry made");
 		return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
 	}
 }
--- src/main/java/com/rakuten/PenguinSoldiers/signin/ForgotPasswordController.java
+++ src/main/java/com/rakuten/PenguinSoldiers/signin/ForgotPasswordController.java
@@ -2,15 +2,22 @@ package com.rakuten.PenguinSoldiers.signin;
 
 import java.security.Principal;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.rakuten.PenguinSoldiers.home.HomeController;
+
 @Controller
 public class ForgotPasswordController {
 
+	public static final Logger logger = Logger.getLogger(HomeController.class);
+	
 	@RequestMapping(value = "forgotPass", method = RequestMethod.GET)
 	public String forgotPass(Principal principal) {
+		
+		logger.info("Entry made for forgotPass");
 		return "signin/forgotPass";
 	}
 
