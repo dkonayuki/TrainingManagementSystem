ea82bacaa6f3b077bee90f6a2fe928a6aca059a9
gUrban <gurban.hudayberdiyev@rakuten.com>
2015-05-07 09:56:49 +0900

    after GW

--- src/main/java/com/rakuten/PenguinSoldiers/signin/ForgotPasswordController.java
+++ src/main/java/com/rakuten/PenguinSoldiers/signin/ForgotPasswordController.java
@@ -12,19 +12,9 @@ import com.rakuten.PenguinSoldiers.home.HomeController;
 @Controller
 public class ForgotPasswordController {
 
-<<<<<<< HEAD
 	@RequestMapping(value = "forgotPassword", method = RequestMethod.GET)
 	public String forgotPass(Principal principal) {
 		return "signin/forgotPassword";
-=======
-	public static final Logger logger = Logger.getLogger(HomeController.class);
-	
-	@RequestMapping(value = "forgotPass", method = RequestMethod.GET)
-	public String forgotPass(Principal principal) {
-		
-		logger.info("Entry made for forgotPass");
-		return "signin/forgotPass";
->>>>>>> a66d84b982d6ca2925e9dda625589b6af0e1dadd
 	}
 
 }
