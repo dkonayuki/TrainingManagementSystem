ab3e0d634834dd5b6aca9e490f6b0ebfa3af2184
michaeljoseph.tan <michaeljoseph.tan@P31275.intra.rakuten.co.jp>
2015-05-08 19:07:42 +0900

    ready to merge

--- src/main/java/com/rakuten/PenguinSoldiers/models/account/AccountRepository.java
+++ src/main/java/com/rakuten/PenguinSoldiers/models/account/AccountRepository.java
@@ -11,28 +11,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Transactional
 public class AccountRepository {
 
-  @PersistenceContext
-  private EntityManager entityManager;
+	@PersistenceContext
+	private EntityManager entityManager;
 
-  @Inject
-  private PasswordEncoder passwordEncoder;
+	@Inject
+	private PasswordEncoder passwordEncoder;
 
-  @Transactional
-  public Account save(Account account) {
-    account.setPassword(passwordEncoder.encode(account.getPassword()));
-    entityManager.persist(account);
-    return account;
-  }
+	@Transactional
+	public Account save(Account account) {
+		account.setPassword(passwordEncoder.encode(account.getPassword()));
+		entityManager.persist(account);
+		return account;
+	}
 
-  public Account findByEmail(String email) {
-    try {
-      return entityManager.createNamedQuery(Account.FIND_BY_EMAIL, Account.class)
-          .setParameter("email", email)
-          .getSingleResult();
-    } catch (PersistenceException e) {
-      return null;
-    }
-  }
+	public Account findByEmail(String email) {
+		try {
+			return entityManager.createNamedQuery(Account.FIND_BY_EMAIL, Account.class)
+					.setParameter("email", email)
+					.getSingleResult();
+		} catch (PersistenceException e) {
+			return null;
+		}
+	}
 
 
 }
