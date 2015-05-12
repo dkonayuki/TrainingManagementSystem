062f1937bda0109f19b61b2d1337d7a67dba387b
gUrban <gurban.hudayberdiyev@rakuten.com>
2015-05-12 12:06:25 +0900

    nothing

---
+++ src/main/java/com/rakuten/PenguinSoldiers/models/training/TrainingForm.java
@@ -0,0 +1,13 @@
+package com.rakuten.PenguinSoldiers.models.training;
+
+public class TrainingForm {
+	private String email;
+
+    public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
