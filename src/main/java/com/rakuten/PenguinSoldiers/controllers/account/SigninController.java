ff1817c1d4f18a82c76b0440d8d1c098419be56c
michaeljoseph.tan <michaeljoseph.tan@P31275.intra.rakuten.co.jp>
2015-05-14 15:16:33 +0900

    update change password

--- src/main/java/com/rakuten/PenguinSoldiers/controllers/account/SigninController.java
+++ src/main/java/com/rakuten/PenguinSoldiers/controllers/account/SigninController.java
@@ -1,12 +1,13 @@
 package com.rakuten.PenguinSoldiers.controllers.account;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SigninController {
 
-	@RequestMapping(value = "signin")
+	@RequestMapping(value = "signin") 
 	public String signin() {
 		return "account/signin";
 	}
