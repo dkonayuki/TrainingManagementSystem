b6c577d8515258154b11693f4d2889d43df931fc
U-INTRA\tianyu.li <tianyu.li@P31391.intra.rakuten.co.jp>
2015-05-15 17:47:20 +0900

    input check

---
+++ src/main/java/com/rakuten/PenguinSoldiers/models/training/TrainingValidator.java
@@ -0,0 +1,14 @@
+package com.rakuten.PenguinSoldiers.models.training;
+
+import java.util.Set;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import javax.validation.executable.ExecutableValidator;
+import javax.validation.metadata.BeanDescriptor;
+
+//public class TrainingValidator implements Validator{
+
+
+//}
