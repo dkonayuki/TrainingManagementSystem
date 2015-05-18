ebcfe2fbbc64bd301ad0e09282c493a688cff5b8
michaeljoseph.tan <michaeljoseph.tan@P31275.intra.rakuten.co.jp>
2015-05-18 12:40:32 +0900

    Merge with

--- src/main/java/com/rakuten/PenguinSoldiers/controllers/home/HomeController.java
+++ src/main/java/com/rakuten/PenguinSoldiers/controllers/home/HomeController.java
@@ -1,7 +1,6 @@
 package com.rakuten.PenguinSoldiers.controllers.home;
 
 import java.security.Principal;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import com.rakuten.PenguinSoldiers.models.account.Account;
 import com.rakuten.PenguinSoldiers.models.account.AccountRepository;
-import com.rakuten.PenguinSoldiers.models.training.Training;
 import com.rakuten.PenguinSoldiers.models.training.TrainingService;
 
 @Controller
@@ -25,20 +23,14 @@ public class HomeController {
 	private TrainingService trainingService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(@RequestParam(value = "filter", required = false, defaultValue = "in") String filter, Principal principal, Model model) {
+	public String index(Principal principal, Model model) {
 		
 		if (principal != null) {
-			UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//			
-			Account user = accountRepository.findByUsername(userDetails.getUsername());
+//			UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			
+//			Account user = accountRepository.findByEmail(userDetails.getUsername());
 //			model.addAttribute("trainings", user.getTrainings());
-		  List<Training> list=trainingService.findRegsiterdTraining(filter.equals("in"),user);
-		  model.addAttribute("trainings", list);
-		  
-		  HomePageContent hpc=new HomePageContent();
-		  hpc.setTrainingTab(filter);
-		  model.addAttribute("hpc", hpc);
-      
+		  model.addAttribute("trainings", trainingService.findActiveTraining());
 			return "home/homeSignedIn";
 		}
 		return "account/signin";
