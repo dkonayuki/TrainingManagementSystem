e64b7dcb35c95a0607906b3256d9c1f83a61e3a0
gUrban <gurban.hudayberdiyev@rakuten.com>
2015-05-19 17:03:37 +0900

    some formatting

--- src/main/java/com/rakuten/PenguinSoldiers/controllers/home/HeaderPageContent.java
+++ src/main/java/com/rakuten/PenguinSoldiers/controllers/home/HeaderPageContent.java
@@ -1,63 +1,59 @@
 package com.rakuten.PenguinSoldiers.controllers.home;
 
-import com.rakuten.PenguinSoldiers.models.account.Account;
-
 public class HeaderPageContent {
-  
-  private boolean isAdmin=false,isManager=false;
-  
-  public static String HOME_LABEL="home";
-  public static String TRAINING_LABEL="training";
-  public static String ACCOUNT_LABEL="account";
-  public static String MODE_LABEL="mode";
-  
-  private String activeLabel="";
-  
-  private String accountName;
-  
-  
-  
-  public String getAccountName() {
-    return accountName;
-  }
 
-  public void setAccountName(String accountName) {
-    this.accountName = accountName;
-  }
+	private boolean isAdmin = false, isManager = false;
 
-  public boolean isAdmin() {
-    return isAdmin;
-  }
+	public static String HOME_LABEL = "home";
+	public static String TRAINING_LABEL = "training";
+	public static String ACCOUNT_LABEL = "account";
+	public static String MODE_LABEL = "mode";
 
-  public void setAdmin(boolean isAdmin) {
-    this.isAdmin = isAdmin;
-  }
+	private String activeLabel = "";
 
-  public boolean isManager() {
-    return isManager;
-  }
+	private String accountName;
 
-  public void setManager(boolean isManager) {
-    this.isManager = isManager;
-  }
-  
-  public void setActiveLabel(String label){
-    this.activeLabel=label;
-  }
-  
-  public String getActiveLabel(){
-    return this.activeLabel;
-  }
-  
-  public boolean isActiveLabel(String label){
-    return this.getActiveLabel().equals(label);
-  }
-  
-  public String showActiveLabel(String label){
-    return this.isActiveLabel(label) ? "active" : "";
-  }
-  
-  public String showAccountTab(){
-    return this.getAccountName()+" <span class=\'caret\'></span> ";
-  }
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public boolean isAdmin() {
+		return isAdmin;
+	}
+
+	public void setAdmin(boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	public boolean isManager() {
+		return isManager;
+	}
+
+	public void setManager(boolean isManager) {
+		this.isManager = isManager;
+	}
+
+	public void setActiveLabel(String label) {
+		this.activeLabel = label;
+	}
+
+	public String getActiveLabel() {
+		return this.activeLabel;
+	}
+
+	public boolean isActiveLabel(String label) {
+		return this.getActiveLabel().equals(label);
+	}
+
+	public String showActiveLabel(String label) {
+		return this.isActiveLabel(label) ? "active" : "";
+	}
+
+	public String showAccountTab() {
+		return this.getAccountName() + " <span class=\'caret\'></span> ";
+	}
 }
