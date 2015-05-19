9e889846e18061f768e976cdb79a23880f722b57
Julian Grinblat <julian@dotcore.co.il>
2015-05-19 10:20:22 +0900

    Parse traning outline

--- src/main/java/com/rakuten/PenguinSoldiers/models/outline/Outline.java
+++ src/main/java/com/rakuten/PenguinSoldiers/models/outline/Outline.java
@@ -1,71 +1,28 @@
 package com.rakuten.PenguinSoldiers.models.outline;
 
-import javax.persistence.*;
+public class Outline {
 
-import com.rakuten.PenguinSoldiers.models.training.Training;
+	private String date;
+	private String text;
 
-import java.sql.Timestamp;
-
-
-@SuppressWarnings("serial")
-@Entity
-@Table(name = "outline")
-@NamedQuery(name = Outline.FIND_BY_ID, query = "select a from Outline a where a.id = :id")
-
-public class Outline implements java.io.Serializable {
-
-	public static final String FIND_BY_ID = "Outline.findById";
-	@Id
-	@GeneratedValue
-	private Long id;
-
-	@ManyToOne
-	@JoinColumn(name = "training_id")
-	private Training training;
-
-	@Lob
-	private String content;
-	private Timestamp date;
-
-	protected Outline() {
-
+	public Outline(String date, String text) {
+		this.date = date;
+		this.text = text;
 	}
 
-	public Outline(String content)
-	{
-		this.content = content;
+	public String getDate() {
+		return this.date;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public Timestamp getDate()
-	{
-		return date;
-	}
-	public void setDate(Timestamp date)
-	{
+	public void setDate(String date) {
 		this.date = date;
 	}
 
-	public Training getTraining() {
-		return training;
+	public String getText() {
+		return this.text;
 	}
 
-	public void setTraining(Training training) {
-		this.training = training;
+	public void setText(String text) {
+		this.text = text;
 	}
-
-
-
 }
-
-
