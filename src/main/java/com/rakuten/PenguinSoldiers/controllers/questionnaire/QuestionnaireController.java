a58bb5897045e35fdb78f8708e6846954abf855d
michaeljoseph.tan <michaeljoseph.tan@P31275.intra.rakuten.co.jp>
2015-05-22 13:45:09 +0900

    resolve conflict

--- src/main/java/com/rakuten/PenguinSoldiers/controllers/questionnaire/QuestionnaireController.java
+++ src/main/java/com/rakuten/PenguinSoldiers/controllers/questionnaire/QuestionnaireController.java
@@ -1,42 +1,23 @@
 package com.rakuten.PenguinSoldiers.controllers.questionnaire;
 
 import java.security.Principal;
-import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.rakuten.PenguinSoldiers.controllers.home.HeaderPageContentBuilder;
-import com.rakuten.PenguinSoldiers.models.account.Account;
+
 import com.rakuten.PenguinSoldiers.models.account.AccountRepository;
-import com.rakuten.PenguinSoldiers.models.account.SignupForm;
-import com.rakuten.PenguinSoldiers.models.account.UserService;
-import com.rakuten.PenguinSoldiers.models.admin.AdminRepository;
-import com.rakuten.PenguinSoldiers.models.goal.Goal;
-import com.rakuten.PenguinSoldiers.models.goal.GoalService;
-import com.rakuten.PenguinSoldiers.models.outline.Outline;
-import com.rakuten.PenguinSoldiers.models.premise.Premise;
-import com.rakuten.PenguinSoldiers.models.target.Target;
+
 import com.rakuten.PenguinSoldiers.models.training.StandardQuestionaire;
 import com.rakuten.PenguinSoldiers.models.training.StandardQuestionaireForm;
 import com.rakuten.PenguinSoldiers.models.training.StandardQuestionaireRepository;
 import com.rakuten.PenguinSoldiers.models.training.Training;
-import com.rakuten.PenguinSoldiers.models.training.TrainingForm;
 import com.rakuten.PenguinSoldiers.models.training.TrainingRepository;
-import com.rakuten.PenguinSoldiers.models.training.TrainingService;
 import com.rakuten.PenguinSoldiers.models.training.TrainingUserRepository;
-import com.rakuten.PenguinSoldiers.models.venue.Venue;
 import com.rakuten.PenguinSoldiers.util.ControllerUtil;
 import com.rakuten.PenguinSoldiers.util.form.QuestionaireConverter;
 
